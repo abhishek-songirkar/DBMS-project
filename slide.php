bde2e6b533e92ee692e4c930571420ebd1ba5301
Rucha-Tanksale <75835899+Rucha-Tanksale@users.noreply.github.com>
2021-01-02 18:40:01 +0530

    slider php file- Rucha

---
+++ slide.php
@@ -0,0 +1,339 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+  <meta charset="utf-8">
+  <title>Welcome page</title>
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
+  <!-- Favicon -->
+  <link rel="icon" href="images/favicon.ico">
+  <!-- GoogleFonts -->
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@1,800&family=Ubuntu:wght@300&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@500&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&family=Open+Sans&family=Roboto+Slab:wght@600&display=swap" rel="stylesheet">
+  <!-- Footer-->   
+  <link rel="stylesheet" href="bots.css">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+
+  <!-- StyleSheets -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  <link rel="stylesheet" href="styles1.css">
+  <!--circle
+  <link rel="stylesheet" type="text/css" href="sy.css">-->
+  <!-- fontAwesome -->
+  <script src="https://kit.fontawesome.com/a0c487d3c9.js" crossorigin="anonymous"></script>
+  <!-- bootstrapScripts -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+
+<!-- cdnjs.com-->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g==" crossorigin="anonymous" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw==" crossorigin="anonymous" />
+
+
+
+<!-- Fonts for Logo -->
+<style type="text/css">
+  @import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Lobster&family=Lobster+Two&family=Satisfy&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=EB+Garamond:wght@500&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Courgette&family=EB+Garamond:wght@500&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Andika+New+Basic&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Acme&display=swap');
+/*Slider  Delete this*/
+
+
+.site-slider{
+  position: relative;
+}
+.position-top{
+  position: absolute;
+  top: 50%;
+}
+.site-slider .slider-btn .prev,
+.site-slider .slider-btn .next{
+  background-color: var(--primary-color);
+  padding: 1rem 1.5rem ;
+  border-radius: 10rem;
+  color: white;
+  margin: 0 1rem;
+
+}
+
+.right-0{
+  right: 0;
+}
+.carousel-cell {
+    width: 100%;
+    }
+
+    /* cell number */
+    .carousel-cell:before {
+      display: block;
+    }
+    /*circle*/
+.imagec{
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+.column {
+  float: left;
+  width: 33.33%;
+  padding: 5px;
+   color: #19456b;
+}
+
+.row::after {
+  content: "";
+  display: table;
+  clear: both;
+  color: #19456b;
+}
+.Para{
+  text-align: center;
+  font-size: 29px;
+  font-family: 'Acme', sans-serif;
+  color: #03506f;
+}
+</style>
+<!-- Slick slider code-->
+
+<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+
+<!-- CSS -->
+<link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
+
+</head>
+
+
+<!-- BODY -->
+
+
+
+<body>
+  
+
+  <section id="testimonials">
+
+
+    <!-- nav bar  -->
+
+
+
+    <nav class="navbar bg-dark navbar-expand-lg fixed-top navbar-dark" style="padding: 3px 15px">
+      <a class="navbar-brand" href="" style="padding-left:10px; font-family: 'Lobster', cursive;">THE ARTSY HOME</a>
+      <form class="form-inline">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+        
+          <li class="nav-item">
+            <a class="nav-link" href="" style="padding:0.5rem 1rem;  color:#fafafa; font-family: 'Andika New Basic', sans-serif;">Contact</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="cofeedback.php" style="padding:0.5rem 1rem; font-family: 'Andika New Basic', sans-serif; color:#fafafa">Corporate Enquiries</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="loginpage.php" style="padding:0.5rem 1rem; font-family: 'Andika New Basic', sans-serif; color:#fafafa">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="signupPage.php" style="padding:0.5rem 1rem; font-family: 'Andika New Basic', sans-serif; color:#fafafa">SignUp</a>
+          </li>
+        
+        </ul>
+      </div>
+      
+    </nav>
+
+
+  <div class="centered">
+    <div class="container-fluid" style="  padding: 130px 50px 50px 50px;">
+      <!-- Title -->
+<!-- /First slider-->
+<h1 class="w3-center" style="margin-top: 10px; color: green; font-family: 'Dancing Script', cursive;">We Turn Your Fantasy Into Fabric</h1>
+<div class="carousel" data-flickity='{ "wrapAround": true, "autoPlay": true }'>
+      <div class="carousel-cell">
+        <img class="w3-image" src="https://smash-images.photobox.com/original/5f04c1b41fd48d1b10ff27dfc90548bf13608845_Large-Print-lifestyle-3_1-2600.jpg">
+      </div>
+      <div class="carousel-cell">
+        <img class="w3-image" src="https://smash-images.photobox.com/original/bca8e5fa7862a2cfaefc300c5b572e7a6dc6f3f3_Standard-Prints-lifestyle-3_1-2600.jpg">
+      </div>
+      <div class="carousel-cell">
+        <img class="w3-image" src="https://smash-images.photobox.com/original/a422aed1a721e933961b19ea9e47e07fc71e0699_Acrylic-Prints-lifestyle-3_1-2600.jpg">
+      </div>
+    </div>
+<!--video for 3d animation https://youtu.be/biCULOcPLRw
+https://youtu.be/woL0YlCqO64-->
+
+<br><br><br><br>
+<!--circle Images
+<h2>After Slider!</h2>-->
+
+  <div class="row">
+    <div class="column">
+
+      <a href="cart.php?type=Window Blinds">
+      <img class="imagec" style="border-radius:100%;" src="images/wBlinds.jpg" width="70%;" height="350px;"></a>
+      <p class="Para">Window Blinds</p>
+    </div>
+
+    <div class="column">
+      <a href="cart.php?type=Upholstery">
+      <img  class="imagec" style="border-radius:100%;" src="images/upholstery.jpg" width="70%;" height="350px;"></a>
+      <p class="Para">Upholstery</p>
+    </div>
+
+    <div class="column">
+      <a href="cart.php?type=Pillows">
+      <img class="imagec" style="border-radius:100%;" src="images/pillows.jpg" width="70%;" height="350px;"></a>
+      <p class="Para">Pillows</p>
+    </div>
+      <br><br><hr>
+    <div class="column">
+      <a href="cart.php?type=Curtains">
+      <img class="imagec" style="border-radius:100%;" src="images/curtains.jpg" width="80%;" height="380px;"></a>
+      <p class="Para">Curtains</p>
+    </div>
+
+    <div class="column">
+      <a href="cart.php?type=Wooden Flooring">
+      <img class="imagec" style="border-radius:100%;" src="images/flooring.jpg" width="80%;" height="380px;"></a>
+      <p class="Para">Wooden Flooring</p>
+    </div>
+
+    <div class="column">
+      <a href="cart.php?type=Wallpaper">
+      <img class="imagec" style="border-radius:100%;" src="images/wallpaper.jpg" width="80%;" height="380px;"></a>
+      <p class="Para">Wallpaper</p>
+    </div>
+  </div>
+
+<br><br><br><br>
+
+<!-- Owl carousel-->
+<div class="owl-carousel owl-theme">
+    <div class="item">
+      <img src="images/sheet2.jpg">
+    </div>
+    <div class="item">
+      <img src="images/rain2.jpg">
+    </div>
+    <div class="item">
+      <img src="images/rain3.jpg">
+    </div>
+    <div class="item">
+      <img src="images/rain.jpg">
+    </div>
+    <div class="item">
+      <img src="images/loftstyle.jpg">
+    </div>
+    <div class="item">
+      <img src="images/sheet2.jpg">
+    </div> 
+   
+</div>
+
+
+<br><br><br><br>
+<!-- Footer-->
+
+  <footer class="footer">
+     <div class="container">
+      <div class="row">
+        <div class="footer-col">
+          <h4>company</h4>
+          <ul>
+            <li><a href="#">about us</a></li>
+            <li><a href="#">our services</a></li>
+            <li><a href="#">privacy policy</a></li>
+            <li><a href="#">affiliate program</a></li>
+          </ul>
+        </div>
+        <div class="footer-col">
+          <h4>Visit Store</h4>
+        <ul>
+        <div class="contact-me">
+        <a class="btn-find" href="https://www.justdial.com/Nashik/Deep-Furnishing-Karmayogi-Chowk-Nashik-Tidke-Nagar/0253PX253-X253-180305001014-M4N4_BZDET">Find Us</a>
+        </div>
+        </ul>
+        </div>
+
+        <div class="footer-col">
+          <h4>Shop</h4>
+          <ul>
+            <li><a href="#">Curtains</a></li>
+            <li><a href="#">Wallpaper</a></li>
+            <li><a href="#">Wooden Flooring</a></li>
+            <li><a href="#">Upholstery</a></li>
+          </ul>
+        </div>
+        <div class="footer-col">
+          <h4>follow us</h4>
+          <div class="social-links">
+          <a href="#"><i class="fas fa-phone-alt"></i></a>
+          <a href="https://www.indiamart.com/deep-furnishing/"><i class="fab fa-chrome"></i></a>
+          </li><a href="https://www.instagram.com/deepfurnishing/"><i class="fab fa-instagram" ></i></a></li>
+      <!--      <a href="#"><i class="fab fa-linkedin-in"></i></a> -->
+          </div>
+
+        </div>
+      </div>
+<br><br><br>
+     <div class="bottom-container">
+      <p class="copyright">© 2021 Team MARS.</p>
+    </div>
+
+       </div>
+  </footer>
+
+
+<!--Slick SLider
+<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+<script src="main.js"></script>-->
+<!-- JavaScript -->
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
+
+  <!-- jquery code-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw==" crossorigin="anonymous"></script>
+<script type="text/javascript">
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:2000,
+    stagePadding: 50,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+</script>
+
+
+
+</body>
+
+</html>
